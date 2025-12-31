6a7305f7e9215775dfa988109b641a0ea9432977
Arun Elanthamil <arun.elanthamil@superops.ai>
2026-01-01 01:45:08 +0530

    [Feat] Docker fix trial.

--- newsletter_project/settings.py
+++ newsletter_project/settings.py
@@ -28,7 +28,8 @@ environ.Env.read_env(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env("SECRET_KEY", default="unsafe-docker-build-key")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
